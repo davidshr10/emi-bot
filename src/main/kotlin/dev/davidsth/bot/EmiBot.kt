7ff0c137b8a5dafdcf05f286805b56cdb09e6949
davidsth <1936038+davidsth@users.noreply.github.com>
2019-03-14 00:52:22 -0400

    Loading COMMANDS on Bot initialization

--- src/main/kotlin/dev/davidsth/bot/EmiBot.kt
+++ src/main/kotlin/dev/davidsth/bot/EmiBot.kt
@@ -2,6 +2,8 @@ package dev.davidsth.bot
 
 import dev.davidsth.bot.config.BotConfig
 import dev.davidsth.bot.config.ConfigUtils
+import dev.davidsth.bot.engine.CommandBuilder
+import dev.davidsth.bot.engine.CommandMap
 import dev.davidsth.bot.listeners.CommandListener
 import dev.davidsth.bot.listeners.ReadyListener
 import net.dv8tion.jda.core.JDABuilder
@@ -10,6 +12,7 @@ object EmiBot {
     private val logger = ConfigUtils.getLogger()
 
     private val config: BotConfig = ConfigUtils.load()
+    val COMMANDS: CommandMap = CommandBuilder().load()
     val name = config.name
     val prefix = config.prefix
 
--- src/test/kotlin/dev/davidsth/bot/EmiBotTest.kt
+++ src/test/kotlin/dev/davidsth/bot/EmiBotTest.kt
@@ -1,6 +1,22 @@
 package dev.davidsth.bot
 
+import dev.davidsth.bot.commands.GithubCommand
+import dev.davidsth.bot.commands.PingCommand
+import net.oddpoet.expect.expect
+import net.oddpoet.expect.extension.contain
 import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
 
 object EmiBotTest: Spek({
+    describe("COMMANDS") {
+        it("loads all the classes in commands package") {
+            val commandNames = EmiBot.COMMANDS.values.map { it::class.simpleName }
+
+            val availableCommands = listOf(PingCommand::class.simpleName, GithubCommand::class.simpleName)
+
+            availableCommands.forEach {
+                expect(commandNames).to.contain(it)
+            }
+        }
+    }
 })
