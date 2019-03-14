ac4f757847205da3254d0865247de05c118f8ff4
davidsth <1936038+davidsth@users.noreply.github.com>
2019-03-14 00:51:44 -0400

    Added CommandBuilder helper to load all annotated classes in commands package

---
+++ src/test/kotlin/dev/davidsth/bot/engine/CommandBuilderTest.kt
@@ -0,0 +1,18 @@
+package dev.davidsth.bot.engine
+
+import net.oddpoet.expect.expect
+import net.oddpoet.expect.extension.containAll
+import org.reflections.Reflections
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+object CommandBuilderTest: Spek({
+    describe("load") {
+        it("returns mapping of annotated classes in commands package") {
+            val classesInPackage = Reflections("dev.davidsth.bot").getSubTypesOf(CommandHandler::class.java).map { it.simpleName }
+            val commandMap = CommandBuilder().load().values.map { it::class.simpleName }
+
+            expect(classesInPackage).to.containAll(commandMap)
+        }
+    }
+})
