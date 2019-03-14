51da35f627b818090855327ca95eabe1b2ed550f
davidsth <1936038+davidsth@users.noreply.github.com>
2019-03-14 00:49:42 -0400

    Added annotation for PingCommand

--- src/main/kotlin/dev/davidsth/bot/commands/PingCommand.kt
+++ src/main/kotlin/dev/davidsth/bot/commands/PingCommand.kt
@@ -1,10 +1,12 @@
 package dev.davidsth.bot.commands
 
 import dev.davidsth.bot.engine.Command
+import dev.davidsth.bot.engine.CommandHandler
 import net.dv8tion.jda.core.entities.MessageChannel
 import net.dv8tion.jda.core.entities.User
 
-class PingCommand : Command {
+@Command("ping")
+class PingCommand : CommandHandler {
     override fun handleMessage(author: User, channel: MessageChannel, message: String) {
         sendMessage(channel, "pong")
     }
